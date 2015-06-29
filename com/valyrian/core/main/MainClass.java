829b3076a3697840a9ebd99fafedde86f41ec091
TheOneAndOnlySulphate <windowsxpprofailiure@gmail.com>
2015-06-29 23:08:51 +0100

    Update MainClass.java
    Added event listener

--- com/valyrian/core/main/MainClass.java
+++ com/valyrian/core/main/MainClass.java
@@ -1,8 +1,11 @@
 package com.valyrian.core.main;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.valyrian.core.commands.AddKeyCommand;
 import com.valyrian.core.commands.LootChestCommand;
+import com.valyrian.core.listeners.OpenMainChest;
 import com.valyrian.core.utils.SendConsoleMessage;
 
 public class MainClass extends JavaPlugin {
@@ -15,7 +18,9 @@ public class MainClass extends JavaPlugin {
 		c.sendMessage(prefix + "ValyrianCore has been enabled!");
 		c.sendMessage(prefix + "The current version is: §bv1.0§a!");
 		plugin = this;
+		Bukkit.getPluginManager().registerEvents(new OpenMainChest(), this);
 		getCommand("lootchest").setExecutor(new LootChestCommand());
+		getCommand("addkey").setExecutor(new AddKeyCommand());
 	}
 	
 	public void onDisable() {
