1e1c4cb08254a297b2643391d958a08d5330f255
TheOneAndOnlySulphate <windowsxpprofailiure@gmail.com>
2015-06-29 21:28:49 +0100

    Updated MainClass.java, added startup msgs

--- com/valyrian/core/main/MainClass.java
+++ com/valyrian/core/main/MainClass.java
@@ -1,19 +1,27 @@
 package com.valyrian.core.main;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.valyrian.core.utils.SendConsoleMessage;
+
 public class MainClass extends JavaPlugin {
-  
-  private static MainClass m;
-  
-  public void onEnable() {
-    m = this;
-  }
-  
-  public void onDisable() {
-    m = null;
-  }
-  
-  public MainClass get() {
-    return m;
-  }
-  
+	
+	SendConsoleMessage c = new SendConsoleMessage();
+	private static MainClass plugin;
+	private String prefix = "§7[§bValyrian§7] §a";
+	
+	public void onEnable() {
+		c.sendMessage(prefix + "ValyrianCore has been enabled!");
+		c.sendMessage(prefix + "The current version is: §bv1.0§a!");
+		plugin = this;
+	}
+	
+	public void onDisable() {
+		plugin = null;
+	}
+	
+	public MainClass get() {
+		return plugin;
+	}
+
 }
