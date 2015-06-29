b38966531cfd5889f5042205117d769dc6b62649
TheOneAndOnlySulphate <windowsxpprofailiure@gmail.com>
2015-06-29 21:05:58 +0100

    Update MainClass.java

--- com/valyrian/core/main/MainClass.java
+++ com/valyrian/core/main/MainClass.java
@@ -2,4 +2,18 @@ package com.valyrian.core.main;
 
 public class MainClass extends JavaPlugin {
   
+  private static MainClass m;
+  
+  public void onEnable() {
+    m = this;
+  }
+  
+  public void onDisable() {
+    m = null;
+  }
+  
+  public MainClass get() {
+    return m;
+  }
+  
 }
