5026d35772ed6a2c3e8c3602c57e99b255c642b8
TheOneAndOnlySulphate <windowsxpprofailiure@gmail.com>
2015-06-30 00:59:10 +0100

    Update AddKeyCommand.java

--- com/valyrian/core/commands/AddKeyCommand.java
+++ com/valyrian/core/commands/AddKeyCommand.java
@@ -1,5 +1,8 @@
 package com.valyrian.core.commands;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -11,6 +14,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 public class AddKeyCommand implements CommandExecutor {
 
+	@SuppressWarnings("deprecation")
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		
 		String prefix = "§7[§bValyrian§7] §a";
@@ -41,8 +45,27 @@ public class AddKeyCommand implements CommandExecutor {
 			return true;
 		}
 		
+		else if (args.length == 1) {
+			List<String> numbers = Arrays.asList("0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20");
+			if (!(numbers.contains(args[0]))) {
+				s.sendMessage(prefix + "§cYou can add a maximum of 20 keys. It has to be a number, not a letter or word.");
+				return true;
+			}
+			Inventory inv = s.getInventory();
+			
+			ItemStack key = new ItemStack(Material.TRIPWIRE_HOOK);
+			ItemMeta keym = key.getItemMeta();
+			keym.setDisplayName("§a§lLootChest Key");
+			key.setItemMeta(keym);
+			for (int i = 0; i < Integer.parseInt(args[0]); i++) {
+				inv.addItem(key);
+			}
+			s.updateInventory();
+			return true;
+		}
+		
 		else {
-			s.sendMessage(prefix + "§cIncorrect command! Usage: /addkey");
+			s.sendMessage(prefix + "§cIncorrect command! Usage: /addkey [amount]");
 			return true;
 		}
 	}
