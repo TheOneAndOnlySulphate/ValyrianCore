c97c7f486eaf2cd71ab8df895fe3fb74e6e632b4
TheOneAndOnlySulphate <windowsxpprofailiure@gmail.com>
2015-06-29 21:18:49 +0100

    Create SendConsoleMessage.java
    Sends a message to the console, § (ChatColor) can be used

---
+++ com/valyrian/core/utils/SendConsoleMessage.java
@@ -0,0 +1,11 @@
+package com.valyrian.core.utils;
+
+import org.bukkit.Bukkit;
+
+public class SendConsoleMessage {
+	
+	public void sendMessage(String message) {
+		Bukkit.getConsoleSender().sendMessage(message);
+	}
+
+}
