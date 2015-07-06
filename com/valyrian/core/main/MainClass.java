3df05c0b2070ea3b21dc0b7b29605cc2202585d5
TheOneAndOnlySulphate <windowsxpprofailiure@gmail.com>
2015-07-06 05:05:19 +0100

    Update MainClass.java

--- com/valyrian/core/main/MainClass.java
+++ com/valyrian/core/main/MainClass.java
@@ -3,32 +3,61 @@ package com.valyrian.core.main;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.gmail.filoghost.holographicdisplays.api.Hologram;
+import com.valyrian.core.commands.AddChestCommand;
 import com.valyrian.core.commands.AddKeyCommand;
 import com.valyrian.core.commands.LootChestCommand;
+import com.valyrian.core.listeners.ChestRewards;
+import com.valyrian.core.listeners.InvClickEventCanceller;
 import com.valyrian.core.listeners.OpenMainChest;
+import com.valyrian.core.utils.LootChestUtils;
 import com.valyrian.core.utils.SendConsoleMessage;
 
 public class MainClass extends JavaPlugin {
 	
 	SendConsoleMessage c = new SendConsoleMessage();
 	private static MainClass plugin;
+	private static LootChestUtils l = new LootChestUtils();
 	private String prefix = "§7[§bValyrian§7] §a";
 	
 	public void onEnable() {
 		c.sendMessage(prefix + "ValyrianCore has been enabled!");
-		c.sendMessage(prefix + "The current version is: §bv1.0§a!");
+		c.sendMessage(prefix + "The current version is: §bv1.3.3§a!");
 		plugin = this;
+	
+		if (getConfig().getBoolean("Chest.Enabled")) {
+			
+			l.createChestHologram();
+			
+		}
+		
 		Bukkit.getPluginManager().registerEvents(new OpenMainChest(), this);
+		Bukkit.getPluginManager().registerEvents(new ChestRewards(), this);
+		Bukkit.getPluginManager().registerEvents(new InvClickEventCanceller(), this);
 		getCommand("lootchest").setExecutor(new LootChestCommand());
 		getCommand("addkey").setExecutor(new AddKeyCommand());
+		getCommand("addchest").setExecutor(new AddChestCommand());
+		
 	}
 	
 	public void onDisable() {
 		plugin = null;
+		c.sendMessage(prefix + "ValyrianCore has been disabled!");
+		
+		if (l.getChestHologram() != null && l.getChestHologram() instanceof Hologram) {
+			
+			l.getChestHologram().delete();
+			
+		}
+		
 	}
 	
 	public static MainClass get() {
 		return plugin;
+	}
+	
+	public static LootChestUtils getLCU() {
+		return l;
 	}
 
 }
