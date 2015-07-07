7f78773c44f268fed47e2a7bbe8a822285d14a9b
xDestx <destroyer7712@gmail.com>
2015-07-07 19:16:36 -0400

    Update FlyCommand.java

--- com/valyrian/core/commands/FlyCommand.java
+++ com/valyrian/core/commands/FlyCommand.java
@@ -30,18 +30,20 @@ public class FlyCommand implements CommandExecutor {
 				if (args[0].equals("enable")) {
 					
 					Player target = (Player) Bukkit.getPlayer(args[1]);
+					target.setAllowFlight(true);
 					target.setFlying(true);
-					target.sendMessage(opxe + " by " + p.getName());
-					p.sendMessage(opxe + " for " + target.getName());
+					target.sendMessage(opxe + "by " + p.getName());
+					p.sendMessage(opxe + "for " + target.getName());
 					
 					return true;
 					
 				} else if (args[0].equals("disable")) {
 					
 					Player target = (Player) Bukkit.getPlayer(args[1]);
+					target.setAllowFlight(false);
 					target.setFlying(false);
-					target.sendMessage(opxd + " by " + p.getName());
-					p.sendMessage(opxd + " for " + target.getName());
+					target.sendMessage(opxd + "by " + p.getName());
+					p.sendMessage(opxd + "for " + target.getName());
 					
 					return true;
 					
@@ -65,13 +67,15 @@ public class FlyCommand implements CommandExecutor {
 				if (args[0].equals("enable")) {
 					
 					p.sendMessage(opxe);
+					p.setAllowFlight(true);
 					p.setFlying(true);
 					return true;
 					
 				} else if (args[0].equals("disable")) {
 					
 					p.sendMessage(opxd);
-					p.setFlying(true);
+					p.setAllowFlight(false);
+					p.setFlying(false);
 					return true;
 					
 				} else if(args[0].equals("?")) { 
@@ -93,12 +97,14 @@ public class FlyCommand implements CommandExecutor {
 				
 			} else if (args.length == 0) {
 				if (p.getAllowFlight() == false) {
+					p.setAllowFlight(true);
 					p.sendMessage(opxe);
 					p.setFlying(true);
 					return true;
 				} else {
+					p.setAllowFlight(false);
 					p.sendMessage(opxd);
-					p.setFlying(true);
+					p.setFlying(false);
 					return true;
 				}
 			} else {
