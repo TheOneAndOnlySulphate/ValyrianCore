016092f28f9188986916def57947d61e368283d5
xDestx <destroyer7712@gmail.com>
2015-07-07 18:38:44 -0400

    Changed "survival" to "Survival"

--- com/valyrian/core/commands/SetGamemodeCommand.java
+++ com/valyrian/core/commands/SetGamemodeCommand.java
@@ -27,34 +27,34 @@ public class SetGamemodeCommand implements CommandExecutor {
 				//If they have the permission or are op
 				Player target = Bukkit.getPlayer(args[1]);
 				
-				if (args[0].equals("s") || args[0].equals("survival") || args[0].equals("0")) {
+				if (args[0].equals("s") || args[0].equals("Survival") || args[0].equals("0")) {
 					
 					target.setGameMode(GameMode.SURVIVAL);
-					target.sendMessage(ChatColor.GREEN + "Gamemode changed to survival by " + p.getName() + "!");
-					p.sendMessage(ChatColor.GREEN + target.getName() + "'s Gamemode was changed to survival.");
+					target.sendMessage(ChatColor.GREEN + "Gamemode changed to Survival by " + p.getName() + "!");
+					p.sendMessage(ChatColor.GREEN + target.getName() + "'s Gamemode was changed to Survival.");
 					
 					return true;
-				}  else if (args[0].equals("c") || args[0].equals("creative") || args[0].equals("1")) {
+				}  else if (args[0].equals("c") || args[0].equals("Creative") || args[0].equals("1")) {
 					
 					target.setGameMode(GameMode.CREATIVE);
-					target.sendMessage(ChatColor.GREEN + "Gamemode changed to creative by " + p.getName() + "!");
-					p.sendMessage(ChatColor.GREEN + target.getName() + "'s Gamemode was changed to creative.");
+					target.sendMessage(ChatColor.GREEN + "Gamemode changed to Creative by " + p.getName() + "!");
+					p.sendMessage(ChatColor.GREEN + target.getName() + "'s Gamemode was changed to Creative.");
 					
 					return true;
-				} else if (args[0].equals("a") || args[0].equals("adventure") || args[0].equals("2")) {
+				} else if (args[0].equals("a") || args[0].equals("Adventure") || args[0].equals("2")) {
 					
 					target.setGameMode(GameMode.ADVENTURE);
-					target.sendMessage(ChatColor.GREEN + "Gamemode changed to adventure by " + p.getName() + "!");
-					p.sendMessage(ChatColor.GREEN + target.getName() + "'s Gamemode was changed to adventure.");
+					target.sendMessage(ChatColor.GREEN + "Gamemode changed to Adventure by " + p.getName() + "!");
+					p.sendMessage(ChatColor.GREEN + target.getName() + "'s Gamemode was changed to Adventure.");
 					
 					return true;
-				} else if (args[0].equals("sp") || args[0].equals("spectator") || args[0].equals("3")) {
+				} else if (args[0].equals("sp") || args[0].equals("Spectator") || args[0].equals("3")) {
 					
 					try {
 						
 						target.setGameMode(GameMode.getByValue(3));
-						target.sendMessage(ChatColor.GREEN + "Gamemode changed to spectator by " + p.getName() + "!");
-						p.sendMessage(ChatColor.GREEN + target.getName() + "'s Gamemode was changed to spectator.");
+						target.sendMessage(ChatColor.GREEN + "Gamemode changed to Spectator by " + p.getName() + "!");
+						p.sendMessage(ChatColor.GREEN + target.getName() + "'s Gamemode was changed to Spectator.");
 						
 						return true;
 					} catch (Exception err) {
@@ -73,30 +73,30 @@ public class SetGamemodeCommand implements CommandExecutor {
 				
 			} else {
 				//If they have the permission or are op
-				if (args[0].equals("s") || args[0].equals("survival") || args[0].equals("0")) {
+				if (args[0].equals("s") || args[0].equals("Survival") || args[0].equals("0")) {
 					
 					p.setGameMode(GameMode.SURVIVAL);
-					p.sendMessage(ChatColor.GREEN + "Gamemode changed to survival!");
+					p.sendMessage(ChatColor.GREEN + "Gamemode changed to Survival!");
 					
 					return true;
-				}  else if (args[0].equals("c") || args[0].equals("creative") || args[0].equals("1")) {
+				}  else if (args[0].equals("c") || args[0].equals("Creative") || args[0].equals("1")) {
 					
 					p.setGameMode(GameMode.CREATIVE);
-					p.sendMessage(ChatColor.GREEN + "Gamemode changed to creative!");
+					p.sendMessage(ChatColor.GREEN + "Gamemode changed to Creative!");
 					
 					return true;
-				} else if (args[0].equals("a") || args[0].equals("adventure") || args[0].equals("2")) {
+				} else if (args[0].equals("a") || args[0].equals("Adventure") || args[0].equals("2")) {
 					
 					p.setGameMode(GameMode.ADVENTURE);
-					p.sendMessage(ChatColor.GREEN + "Gamemode changed to adventure!");
+					p.sendMessage(ChatColor.GREEN + "Gamemode changed to Adventure!");
 					
 					return true;
-				} else if (args[0].equals("sp") || args[0].equals("spectator") || args[0].equals("3")) {
+				} else if (args[0].equals("sp") || args[0].equals("Spectator") || args[0].equals("3")) {
 					
 					try {
 					
 						p.setGameMode(GameMode.getByValue(3));
-						p.sendMessage(ChatColor.GREEN + "Gamemode changed to spectator!");
+						p.sendMessage(ChatColor.GREEN + "Gamemode changed to Spectator!");
 						
 						return true;
 					
