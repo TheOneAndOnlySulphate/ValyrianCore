c509d78cf7b7e929716b38711a7a63dcf9716979
AppleMC <uAppleMC@gmail.com>
2015-07-07 21:37:23 -0400

    Update MainClass.java

--- com/valyrian/core/main/MainClass.java
+++ com/valyrian/core/main/MainClass.java
@@ -4,13 +4,17 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.gmail.filoghost.holographicdisplays.api.Hologram;
-import com.valyrian.core.commands.AddChestCommand;
-import com.valyrian.core.commands.AddKeyCommand;
-import com.valyrian.core.commands.LootChestCommand;
-import com.valyrian.core.listeners.ChestRewards;
-import com.valyrian.core.listeners.InvClickEventCanceller;
-import com.valyrian.core.listeners.OpenMainChest;
-import com.valyrian.core.utils.LootChestUtils;
+import com.valyrian.core.essentials.commands.BanCommand;
+import com.valyrian.core.essentials.commands.FlyCommand;
+import com.valyrian.core.essentials.commands.KickCommand;
+import com.valyrian.core.essentials.commands.SetGamemodeCommand;
+import com.valyrian.core.lootchest.commands.AddChestCommand;
+import com.valyrian.core.lootchest.commands.AddKeyCommand;
+import com.valyrian.core.lootchest.commands.LootChestCommand;
+import com.valyrian.core.lootchest.listeners.ChestRewards;
+import com.valyrian.core.lootchest.listeners.InvClickEventCanceller;
+import com.valyrian.core.lootchest.listeners.OpenMainChest;
+import com.valyrian.core.lootchest.utils.LootChestUtils;
 import com.valyrian.core.utils.SendConsoleMessage;
 
 public class MainClass extends JavaPlugin {
@@ -34,12 +38,17 @@ public class MainClass extends JavaPlugin {
 		Bukkit.getPluginManager().registerEvents(new OpenMainChest(), this);
 		Bukkit.getPluginManager().registerEvents(new ChestRewards(), this);
 		Bukkit.getPluginManager().registerEvents(new InvClickEventCanceller(), this);
+		
+		// LootChest Commands
 		getCommand("lootchest").setExecutor(new LootChestCommand());
 		getCommand("addkey").setExecutor(new AddKeyCommand());
 		getCommand("addchest").setExecutor(new AddChestCommand());
+		
+		// Essentials Commands
 		getCommand("gamemode").setExecutor(new SetGamemodeCommand());
 		getCommand("gm").setExecutor(new SetGamemodeCommand());
 		getCommand("fly").setExecutor(new FlyCommand());
+		getCommand("ban").setExecutor(new BanCommand());
 		getCommand("kick").setExecutor(new KickCommand());
 		
 	}
