e0545d42cf159656a4ee9158069fac2e01ed715f
TheOneAndOnlySulphate <windowsxpprofailiure@gmail.com>
2015-07-08 21:16:11 +0100

    Update MainClass.java
    Fully reload friendly, changed imports

--- com/valyrian/core/main/MainClass.java
+++ com/valyrian/core/main/MainClass.java
@@ -5,22 +5,12 @@ import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.gmail.filoghost.holographicdisplays.api.Hologram;
-import com.valyrian.core.essentials.commands.BanCommand;
-import com.valyrian.core.essentials.commands.BanIPCommand;
-import com.valyrian.core.essentials.commands.ClearInventoryCommand;
-import com.valyrian.core.essentials.commands.EnderChestCommand;
-import com.valyrian.core.essentials.commands.FlyCommand;
-import com.valyrian.core.essentials.commands.HealCommand;
-import com.valyrian.core.essentials.commands.KickCommand;
-import com.valyrian.core.essentials.commands.SetGamemodeCommand;
-import com.valyrian.core.essentials.commands.WorkbenchCommand;
-import com.valyrian.core.lootchest.commands.AddChestCommand;
-import com.valyrian.core.lootchest.commands.AddKeyCommand;
-import com.valyrian.core.lootchest.commands.LootChestCommand;
-import com.valyrian.core.lootchest.listeners.ChestRewards;
-import com.valyrian.core.lootchest.listeners.InvClickEventCanceller;
-import com.valyrian.core.lootchest.listeners.OpenMainChest;
-import com.valyrian.core.lootchest.utils.LootChestUtils;
+import com.valyrian.core.essentials.commands.*;
+import com.valyrian.core.essentials.listeners.*;
+import com.valyrian.core.essentials.utils.*;
+import com.valyrian.core.lootchest.commands.*;
+import com.valyrian.core.lootchest.listeners.*;
+import com.valyrian.core.lootchest.utils.*;
 import com.valyrian.core.utils.SendConsoleMessage;
 
 public class MainClass extends JavaPlugin {
@@ -45,6 +35,8 @@ public class MainClass extends JavaPlugin {
 			
 		}
 		
+		
+		//LootChest
 		Bukkit.getPluginManager().registerEvents(new OpenMainChest(), this);
 		Bukkit.getPluginManager().registerEvents(new ChestRewards(), this);
 		Bukkit.getPluginManager().registerEvents(new InvClickEventCanceller(), this);
@@ -52,18 +44,16 @@ public class MainClass extends JavaPlugin {
 		getCommand("addkey").setExecutor(new AddKeyCommand());
 		getCommand("addchest").setExecutor(new AddChestCommand());
 		
-		// Essentials Commands
+		//Essentials
+		Bukkit.getPluginManager().registerEvents(new PlayerJoinMessage(), this);
 		getCommand("gamemode").setExecutor(new SetGamemodeCommand());
-		getCommand("gm").setExecutor(new SetGamemodeCommand());
 		getCommand("fly").setExecutor(new FlyCommand());
 		getCommand("ban").setExecutor(new BanCommand());
 		getCommand("kick").setExecutor(new KickCommand());
 		getCommand("heal").setExecutor(new HealCommand());
 		getCommand("workbench").setExecutor(new WorkbenchCommand());
 		getCommand("clearinventory").setExecutor(new ClearInventoryCommand());
-		getCommand("ci").setExecutor(new ClearInventoryCommand());
 		getCommand("banip").setExecutor(new BanIPCommand());
-		getCommand("chest").setExecutor(new EnderChestCommand());
 		
 	}
 	
@@ -85,6 +75,10 @@ public class MainClass extends JavaPlugin {
 	
 	public static LootChestUtils getLCU() {
 		return l;
+	}
+	
+	public static String getPrefix() {
+		return prefix;
 	}
 
 }
