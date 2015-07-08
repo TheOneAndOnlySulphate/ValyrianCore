1635b36b55c6ece45f4aeb0bc935608d4e67b0cb
TheOneAndOnlySulphate <windowsxpprofailiure@gmail.com>
2015-07-08 14:32:54 +0100

    Create PlayerJoinMessage.java (WIP)
    Not finished, please leave it though

---
+++ com/valyrian/core/essentials/listeners/PlayerJoinMessage.java
@@ -0,0 +1,21 @@
+package com.valyrian.core.essentials.listeners;
+
+public class PlayerJoinMessage implements Listener {
+
+    @EventHandler
+    public void onEvent(PlayerJoinEvent e) {
+    
+        Player p = e.getPlayer();
+        
+        //TODO: Finish
+    
+    }
+    
+    public void checkIfStaff(Player p) {
+    
+        //TODO
+    
+    }
+
+
+}
