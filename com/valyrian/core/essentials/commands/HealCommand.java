33739312e6f7ae556022b09641878f73218f3852
AppleMC <uAppleMC@gmail.com>
2015-07-07 22:01:42 -0400

    Update HealCommand.java

--- com/valyrian/core/essentials/commands/HealCommand.java
+++ com/valyrian/core/essentials/commands/HealCommand.java
@@ -1,4 +1,4 @@
-package com.valyrian.core.commands;
+package com.valyrian.core.essentials.commands;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -10,34 +10,65 @@ import org.bukkit.entity.Player;
 
 public class HealCommand implements CommandExecutor {
 	
+	String prefix = "§7[§bValyrian§7] §a";
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		Player p = (Player) sender;
-		String msg = ChatColor.GOLD + "You have been healed.";
-		String smsg = ChatColor.GOLD + "Healed.";
-		String allheal = ChatColor.GREEN + "All heal!";
-		if (!p.hasPermission("vc.heal"))
-			return false;
 		
-		if(args.length == 0) {
-			p.setHealth(p.getMaxHealth());
-			p.sendMessage(msg);
-		} else if (args.length == 1) {
-			if (p.hasPermission("vc.heal.others")) {
-				if (args[0].equals("all") && (Bukkit.getPlayer(args[0]) == null)) {
-					for (Player player : Bukkit.getOnlinePlayers()) {
-						player.setHealth(player.getMaxHealth());
-						player.sendMessage(allheal + " " + msg);
+		if (!p.hasPermission("vc.heal")) {
+			
+			p.sendMessage(prefix + "§cYou do not have permissions to do this!");
+			
+			return true;
+			
+		} else {
+			
+			if (args.length == 0) {
+				
+				p.setHealth(p.getMaxHealth());
+				
+				p.sendMessage(prefix + "You have been healed.");
+				
+				return true;
+				
+			} else if (args.length == 1) {
+				
+				if (!p.hasPermission("vc.heal.others")) {
+					
+					if (args[0].equalsIgnoreCase("all")) {
+						
+						for (Player player : Bukkit.getOnlinePlayers()) {
+							
+							player.setHealth(player.getMaxHealth());
+							
+							player.sendMessage(prefix + "You have been healed by §b" + p.getDisplayName() + "§a.");
+							
+						}
+						
+						p.sendMessage(prefix + "You have healed everyone on the server.");
+						
+					} else {
+						
+						Player target = Bukkit.getPlayer(args[0]);
+						
+						if (target != null) {
+							
+							target.setHealth(target.getMaxHealth());
+							
+							target.sendMessage(prefix + "You have been healed by §b" + p.getDisplayName() + "§a.");
+							
+							p.sendMessage(prefix + "You have healed §b" + target.getDisplayName() + "§a.");
+							
+						}
+						
 					}
-				} else {
-					Player target = Bukkit.getPlayer(args[0]);
-					target.setHealth(target.getMaxHealth());
-					target.sendMessage(msg);
-					p.sendMessage(smsg);
+					
 				}
+				
 			}
+			
 		}
-		
 		
 		return true;
 	}
