ae8a3dbf59fbdc5dce6f91deb8dd0c714d768f42
AppleMC <uAppleMC@gmail.com>
2015-07-13 18:54:03 -0400

    Update InvSeeCommand.java

--- com/valyrian/core/essentials/commands/InvSeeCommand.java
+++ com/valyrian/core/essentials/commands/InvSeeCommand.java
@@ -1,12 +1,22 @@
 package com.valyrian.core.essentials.commands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import com.valyrian.core.main.MainClass;
+
 public class InvSeeCommand implements CommandExecutor {
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         
         String prefix = MainClass.getPrefix();
     
-        if (!(sender instanceof Player) {
+        if (!(sender instanceof Player)) {
             
             sender.sendMessage(prefix + "§cOnly a player can run this command!");
             return true;
@@ -18,7 +28,7 @@ public class InvSeeCommand implements CommandExecutor {
         if (args.length < 1) {
             
             s.sendMessage(prefix + "§cNot enough arguments!");
-            s.sendmessage(prefix + "§cUsage: §a/invsee <player>")
+            s.sendMessage(prefix + "§cUsage: §a/invsee <player>");
             return true;
             
         }
@@ -29,7 +39,7 @@ public class InvSeeCommand implements CommandExecutor {
             
             if (t == null) {
                 
-                s.sendMessage(prefix + "§cThat player does not exist, or is not online.")
+                s.sendMessage(prefix + "§cThat player does not exist, or is not online.");
                 return true;
                 
             }
@@ -47,6 +57,8 @@ public class InvSeeCommand implements CommandExecutor {
             s.openInventory(ninv);
             
         }
+        
+		return true;
     
     }
 
